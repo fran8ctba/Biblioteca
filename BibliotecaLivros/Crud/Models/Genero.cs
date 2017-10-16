f7c22faf35d873ad8451af73aabaa370f82b35ba
LAPTOP-GHO5MFDP\Fran <fran8ctba@hotmail.com>
2017-10-16 09:08:17 -0200

    Livraria Primeira Versao

---
+++ BibliotecaLivros/Crud/Models/Genero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
